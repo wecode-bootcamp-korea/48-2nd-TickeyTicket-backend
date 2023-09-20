6fc336c8600a97c38f0e8e175c80c3f816ff150a
ghj1 <ghj12222@gmail.com>
2023-09-20 10:49:26 +0900

    [feature]Main Show ProductList

---
+++ api/controllers/mainController.js
@@ -0,0 +1,39 @@
+const mainService = require('../services/mainService');
+const { catchAsync } = require('../utils/error');
+
+const getProductList = catchAsync(async(req, res)  => {
+    const userId = req.user ? req.user.id : null;
+    const { lng, lat, genreId, hashtagId, dateBy, sortBy } = req.query;
+
+    const showproductlist = await mainService.getProductByCategory(
+        userId,
+        lng,
+        lat,
+        genreId, 
+        hashtagId, 
+        dateBy,
+        sortBy
+    );
+    
+    showproductlist.forEach((item) => {
+        item.latlng = JSON.parse(item.latlng);
+    });
+
+    res.status(201).json( { data: showproductlist } );
+})
+
+const getAllProdctList = catchAsync(async(req, res) => {
+    const showAllProdcut = await mainService.getAllProdctList();
+
+    showAllProdcut.forEach((item) => {
+        item.latlng = JSON.parse(item.latlng);
+    });
+
+    res.status(201).json( showAllProdcut );
+})
+
+module.exports = {
+    getProductList,
+    getAllProdctList
+};
+
